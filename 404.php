52feb41fc9e63cfa8ff6983b035eebd612c57645
rweil55 <github@shaw-weil.com>
2025-02-25 16:37:47 -0500

    first commit

---
+++ 404.php
@@ -0,0 +1,129 @@
+<?php
+
+/**
+ * The template for displaying 404 pages (Not Found)
+ *
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
+ */
+global $eol;
+$debug404 = false;
+if ($debug404) ini_set('display_errors', 1);
+$urlarray = parse_url($_SERVER["REQUEST_URI"]);
+if ($debug404) {
+    var_dump($urlarray);
+    print $eol;
+}
+$scheme = $_SERVER["REQUEST_SCHEME"];
+$host = $_SERVER['SERVER_NAME'];
+if ($debug404) print "host is $host $eol";
+$path = $urlarray["path"];
+if ($debug404) print "path is $path $eol";
+$query = $_SERVER["QUERY_STRING"];
+$url = $_SERVER["REQUEST_URI"];
+$pathinfo = pathinfo($path);
+$filename = $pathinfo["filename"];
+$direname = $pathinfo["dirname"];
+if ("/" == $direname) {
+    $direname = "";
+}
+if ($debug404) print "directory - $direname, file - $filename, ABSPATH " . ABSPATH . $eol;
+$pageSlug = str_replace("/", "", $filename);
+if ($debug404) print "page slug is $pageSlug $eol";
+$sqlPage = "SELECT * FROM " . $table_prefix . "posts where post_type = 'page' and post_status='publish' and post_name = '$pageSlug' ";
+$result = $wpdb->get_results($sqlPage);
+if ($debug404) print "sql is $sqlPage $eol";
+if ($wpdb->num_rows > 0) {
+    $newRequest = $pageSlug;
+    if ($debug404) print "page slug $filename exists $eol";
+    header("Location: $newRequest");
+} else {
+    if ($debug404) print "page slug $filename does not exist $eol";
+}
+$possibleExt = array("htm", "php", "html");
+foreach ($possibleExt as $newExt) {
+    $file = "$filename.$newExt";
+    if (!empty($direname)) {
+        $fileLoc = ABSPATH . "$direname$file";
+        if ($debug404) print "added dire $eol";
+    } else {
+        $fileLoc = ABSPATH . "$file";
+        if ($debug404) print "no dire $eol";
+    }
+    if ($debug404) print "file location is $fileLoc $eol";
+    if (file_exists($fileLoc)) {
+        $newRequest = "$file";
+        if (!empty($query)) {
+            $newRequest .= "?$query";
+        }
+        if ($debug404) print "new request is $newRequest $eol";
+        //   header("Location: $newRequest");
+        //  }
+    } // end  if ( $ext == $extTest )
+    // -------------------------------------------------------- //
+    if ("edit.shaw-weil.com" == $host && empty($query)) {
+        $debug404 = true;
+        if ($debug404) {
+            print "<strong>special code</strong> for edit.shaw-weil.com : host is $host ,path is $path ,query is $query ,url is $url $eol";
+            print "filename is $filename ,direname is $direname ,page slug is $pageSlug ,sql is $sqlPage ,sql is $sqlTrail $eol";
+        }
+        // see if it is just a trail name as the page slug
+        //      if so redirect to the page with ?trailId=page slug
+        $sqlTrail = "SELECT * FROM  $wpdbExtra->trails where trId = '$pageSlug' ";
+        $result = $wpdb->get_results($sqlTrail);
+        if ($debug404) print "sql is $sqlTrail $eol";
+        if ($wpdb->num_rows > 0) {
+            $newRequest = "$direname/?trailid=$pageSlug";
+            if ($debug404) print "lets try $newRequest  $eol";
+            header("Location: $newRequest");
+            return true;
+        } // end if ($wpdb->num_rows > 0)
+        // see if is just a fix task name as the page slug
+        //      if so redirect to the page with ?task=page slug
+        if ($sebug404) print "direname is $direname try a fix task $eol";
+        if ("/fix" == substr($direname, 0, 4) && empty($query)) {
+            $newRequest = "$direname/?task=$pageSlug";
+            if ($debug404) print "lets try $newRequest  $eol";
+            header("Location: $newRequest");
+            return true;
+        } // end if ("/fix" == substr($pageslug, 0, 3))
+        if ("task=" == substr($pageSlug, 0, 5)) {
+            $newRequest = "$direname/?$pageSlug";
+            if ($debug404) print "lets try $newRequest  $eol";
+            header("Location: $newRequest");
+            return true;
+        } // end if ("/task" == substr($pageslug, 0, 5))
+
+    }
+} // end for extMain
+get_header();
+?>
+<div id="primary" class="content-area">
+    <div id="content" class="site-content" role="main">
+        <header class="page-header">
+            <h1 class="rrw-title">
+                <?php _e("$url was not Found on site", 'twentythirteen'); ?>
+            </h1>
+        </header>
+        <div class="page-wrapper">
+            <div class="page-content">
+                <h2>
+                    <?php _e('This is somewhat embarrassing, isn&rsquo;t it?', 'twentythirteen'); ?>
+                </h2>
+                <p>
+                    <?php _e('It looks like nothing was found at this location. Maybe try a search?', 'twentythirteen'); ?>
+                </p>
+                <?php get_search_form(); ?>
+                <div id="wb404" />
+                <script src="https://archive.org/web/wb404.js"> </script>
+            </div>
+        </div>
+        <!-- .page-content -->
+    </div>
+    <!-- .page-wrapper -->
+</div>
+<!-- #content -->
+</div>
+<!-- #primary -->
+<?php get_footer(); ?>
---
+++ header.php
@@ -0,0 +1,486 @@
+<?php
+/*
+ * The Header template for our theme
+ *
+ * Displays all of the <head> section and everything up till <div id="main">
+ *
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
+ */
+/* 	this is Roy Weil's standard Trail header
+ *
+ *	to use: do the following
+ *	Under "appearance", "customize", "Header Image" upload the left hand header logo
+ *	   at a height of about 150
+ *	Under "appearance", "customize", "Site Identity" enter header text
+ *	Upload right hand header images to the directory wp-content/upload/Top-Banner-Images
+ *			if no right hand image wanted, leave direcory empty
+ *
+ */
+
+$debugSwitch = false;
+$eol = "<br />\n";
+//if ( current_user_can( "edit_users" ) ) {
+ini_set("display_errors", true);
+//} // else no auto debug
+?>
+<!DOCTYPE html>
+<!--[if IE 7]>
+<html class="ie ie7" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 7) | !(IE 8)  ]><!-->
+<html <?php language_attributes(); ?>>
+<!--<![endif]-->
+
+<head>
+    <!-- page created by header.php try #4 -->
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width">
+    <title>
+        <?php
+        wp_title('|', true, 'right');
+        ?>
+    </title>
+    <!--	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+-->
+    <!--[if lt IE 9]>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
+	<![endif]-->
+    <?php
+    wp_head();
+    $rrw_trail_menu_footer_background_color =
+        get_option("freewheelingeasy_menu_footer_background_color", "black");
+    $rrw_trail_menu_footer_text_color =
+        get_option("freewheelingeasy_menu_footer_text_color", "white");
+    // switchname used to select different header based on url
+    $switchName = rrw_trail_SetSwitshName();
+    print "\n<!-- themes style section based on url and customizations  - $switchName -->\n";
+    switch ($switchName) { // set styles base on switch
+        case "eriepittsburgh":
+            print "
+<style>
+div.erieswishlogo {
+	position: relative;
+	top: 0;
+	left: 0px;
+}
+div.erieSearchForm {
+	position: absolute;
+	bottom: 5px;
+	right: 0px;
+	border: 2px solid #73AD21;
+}
+div.eriemenu {
+	position: absolute;
+    top:45px;
+	left: 125px;
+	height: 20px;
+# border: 2px solid #73AD21;
+	z-index: 100;
+}
+/* Mobile devices */
+@media (max-width: 359px) {
+}
+<style>
+    ";
+            break;
+        case "picture":
+        case "picturedev":
+            print "
+<script src='/wp-content/plugins/roys-picture-processng/pictures.js'></script>
+<script src='https://pictures.shaw-weil.com/wp-includes/js/jquery/jquery.min.js?ver=3.6.0'
+    id='jquery-core-js'></script>
+<script src='https://pictures.shaw-weil.com/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2'
+    id='jquery-migrate-js'></script>
+<link rel='stylesheet'
+    href='https://pictures.shaw-weil.com/wp-content/plugins/roys-picture-processng/pictures.css' media='all'
+    id='picture.csss' ></link>
+<style >
+.nav_rrw_buttons {
+    display: block;
+    matgin-left: auto;
+    margin-right: auto;
+}
+</style>
+";
+            break;
+        case "dino1":
+    ?>
+            <style>
+                .dinoMenu {
+                    color: black;
+                    background-color: orange;
+                    text-align: left;
+                    line-height: 1.2;
+                }
+
+                .dinoMenu a {
+                    color: white;
+                }
+
+                .dinoMenu strong {
+                    color: black;
+                }
+            </style>
+        <?php
+            break;
+        case "theyworking":
+        case "tommarellogc":
+            $rrw_trail_menu_footer_text_color = "white";
+            break;
+        case "normal":
+        case "clean":
+        case "dino":
+        case "linkup":
+        case "tailonly":
+        case "edit":
+            break;
+        default:
+            print "<p>E#1302 Unkown switchName of '$switchName' in header.php</p> ";
+            break;
+    }
+    // end f ( $switchName == "eriepittsburgh" )
+    print "
+<style>
+.menucolor {
+    color: $rrw_trail_menu_footer_text_color;
+    background-color: $rrw_trail_menu_footer_background_color;
+    min-height: 26px;
+}
+.menuitem {
+    color: $rrw_trail_menu_footer_text_color;
+    background-color: $rrw_trail_menu_footer_background_color;
+    min-height: 26px;
+}
+.nav-menu a {
+        color: $rrw_trail_menu_footer_text_color!important;
+        background-color: $rrw_trail_menu_footer_background_color;
+        min-height: 26px;
+    }
+    .nav-menu .current_page_item > a, .nav-menu .current_page_ancestor > a, .nav-menu .current-menu-item > a, .nav-menu .current-menu-ancestor > a {
+        color: $rrw_trail_menu_footer_background_color!important;
+        background-color: $rrw_trail_menu_footer_text_color;
+    }
+    .site-footer a {
+        color: $rrw_trail_menu_footer_text_color!important;
+    }
+</style>
+<!-- end themes style section based on url and customizations -->
+<script src='https://pictures.shaw-weil.com/randomTrailPicture.js'></script>
+</head>
+<body ";
+    body_class();
+    print ">
+    <div id='page' class='hfeed site'>
+        <!--  ==================================================================================================== header -->
+        <header id='masthead' style='text-align:left;'>
+            <a class='screen-reader-text skip-link' href='#content' aria-label='";
+    esc_attr_e(' Skip to content', 'twentythirteen');
+    print "' title='";
+    esc_attr_e('Skip to content', 'twentythirteen');
+    print "'> </a>\n";
+    $image = get_header_image();
+    $homeName = esc_attr(get_bloginfo('name', 'display'));
+    if (array_key_exists("nohead", $_GET)) $noheader = true;
+    else $noheader = false;
+    if ($noheader)
+        return;
+    $mobile = false;
+    if (array_key_exists('HTTP_USER_AGENT', $_SERVER)) {
+        $browser = $_SERVER['HTTP_USER_AGENT'];
+        if (strpos($browser, "Mobile") !== false || strpos($browser, "Samsung"))
+            $mobile = true;
+    }
+    $rightRandomImage = ""; // the various switch routines will set this if they want it
+    $searchbox = "";        // the various switch routines will set this if they want it
+    if ($mobile && (0 != strcmp("dino1", $switchName))) {
+        print "<!-- mobile devices do not get the images -->\n";
+    } else {
+        switch ($switchName) {
+            case "eriepittsburgh": // -------------------------------------------- header erie
+                $imageSource = get_bloginfo('stylesheet_directory') . "/images/cropped-swishlogo.jpg";
+                print "
+            <!-- start div id='rrw_header_menu_block_1' -->
+<div id='rrw_header_menu_block' class='erieswishlogo'>
+    <table id='rrw_header_mastheadPhotos' style='min-height: 30px; border:2px' role='presentation'>
+        <tr>
+            <td><img src='$imageSource' class='alignnone size-full' ></td>
+        </tr>
+    </table>
+    <div id='rrw_header_searchform' class='erieSearchForm'>";
+                get_search_form();
+                print " </div>
+    <!-- end /div id='rrw_header_searchform'  --> \n
+</div>
+            <!-- end /div id='rrw_header_menu_block_1' --> ";
+                break;
+                /*  // picture has it own special header, does not use this code
+            case "picture":
+            case "picturedev":
+                if ($debugSwitch) print "working on picture header $eol";
+                if ("picturedev" == $switchName)
+                    $dev = "-dev";
+                else
+                    $dev = "";
+                $pictureSearchBox =
+                    "/home/pillowan/www-shaw-weil-pictures$dev/wp-content/plugins" .
+                    "/roys-picture-processng/searchBox.php";
+                if (!file_exists($pictureSearchBox)) {
+                    print "E#1300 looking for the file $pictureSearchBox,
+                        need to load the pictures plugin
+                            <br />  --- Fatal error ";
+                    exit();
+                }
+                require_once "$pictureSearchBox";
+                if ($debugSwitch) print "got the code $eol ";
+                $box = rrwPictures_searchBox::rrwPicturesSearchBox();
+                if ($debugSwitch) print "rrwPicturesSearchBox returned, " . strlen($box) . "
+                        bytes $eol ";
+                print $box;
+                break;
+                */
+            case "theyworking":
+                print '
+    <table border="0" id="table2" cellspacing="0" cellpadding="0"
+    style=\'width:100%; border:0;
+background-image:url("/wp-content/themes/roys-header/images/riders-header-1-1700.jpg")\' >
+        <tr>
+            <td align="center" style="headerTD">
+		<h3 align="center"><font color="white">
+		<span style="font-size: 28pt">
+		Trail Volunteer Fund<br></span></font>
+        <font size="5" color="white">of The Pittsburgh Foundation</font></h3>
+        <h3 align="center"><span style="font-weight: 400"><font color="white"><i>
+	Supporting volunteer trail projects with tools and materials</i></font></span></h3>
+    <span align="center" >
+    <!-- navigation buttons -->
+       <map name="FPMap0_I1">
+		<area href="/" shape="rect" coords="4, 0, 172, 44"
+				alt="link to Home page">
+		<area href="/projects/" shape="rect" coords="192, 0, 360, 44"
+				alt="Link to projects page">
+		<area href="/grants/" shape="rect" coords="378, 0, 545, 44"
+				alt="Link to ho to make a grant page">
+		<area href="/helpus/" shape="rect" coords="566, 0, 735, 44"
+				alt="Link to How to make a contribution page">
+		</map>
+        <div style=\'align-centr\'>
+		<img class="nav_rrw_buttons" src="/wp-content/themes/roys-header/images/sign-buttons-v2.gif" usemap="#FPMap0_I1" alt="navigation buttons" width="735" height="44" >
+        </div>
+    <!-- navigation buttons -->
+    </span>
+</td>
+</tr>
+</table>
+        ';
+                break;
+            case "normal":
+            case "linkup":
+                // build sme variables
+                $LogoImage = get_header_image();
+                $rightRandomImage = "<div id='randomTrailImageGoesHereDiv'>one moment while we fetch a trail picture
+                <script>
+                    randomPicFunction('randomTrailImageGoesHereDiv');
+                </script>
+            </div>";
+                $searchbox = get_search_form(array("echo" => false));
+                $desc = get_bloginfo('description');
+                $siteUrl = site_url();
+                if (empty($desc)) {
+                    // empty description - increase size of title
+                    print "
+<style>
+    .site-title { font-size: 36px;  }
+</style>
+";
+                }
+                print "
+<div id='rrw_header_menu_block'>
+    ";
+                if (false === strpos($LogoImage, "White_48-x-40.png")) {
+                    // we have a logo image.
+                    print "
+    <table id='rrw_header_mastheadPhotos' style='min-height: 30px;
+            border: 2px; ' role='presentation'>
+        <tr>
+            <td><a class='home-link site-description' href='$siteUrl' title='$homeName' rel='home'>
+                    <img src='$LogoImage' alt='$homeName logo ' class='alignnone size-full' > </a>
+            </td>
+            <td style='text-align:center; border:thin;'>
+               <a href='$siteUrl' title='$homeName' rel='home'>
+                <h1 class='site-title'>$homeName</h1>
+                <h2 class='site-description'> $desc</h2>
+                </a>
+            </td>
+            <td class='site-description'>
+            $rightRandomImage
+            $searchbox
+            </td>
+        </tr>
+    </table>
+    ";
+                } else {
+                    // no logo image just the search box
+                    print '
+    <span class="screen-reader-text">Search for:</span>
+    <input type="search" class="search-field" placeholder="Search &hellip;" value="" name="s" />
+    </label>
+    <input type="submit" class="search-submit" value="Search" />
+    </form>
+    </td>
+    </tr>
+    </table>
+    ';
+                }
+                print "</div> <!-- end /div id='rrw_header_menu_block_3' -->";
+                break;
+            case "dino":
+            case "clean":
+            case "tailonly":
+            case "edit":
+                print "<!-- no header displayed -->";
+                break;
+            case "tommarellogc":
+                print '
+        <img src="https://tommarellogc.com/wp-content/uploads/2014/09/tommarelloLogo.jpg" >
+';
+                break;
+            case "dino1":
+                $title = wp_title("", false);
+                print "<div id=dinoMenu class='dinoMenu' > <!-- entire space is orange -->
+            <table class='dinoMenu' style='table-layout: auto;' >
+       <tr class='dinoMenu' >
+            <td><span class='site-title' > $title </span><br /><br />\n";
+                $siteDire = "/home/pillowan/www-dinomitedays";
+                $contentLoc = "$siteDire/wp-content/plugins/dinomitedays/footer_dino1.php";
+                include "$contentLoc";
+                print "
+            </td>
+            ";
+                if ($mobile) { // on mobile devices do not display the logo
+                    print "
+            <td> </td>
+            ";
+                } else {
+                    print "
+        <td class='dinoMenu' >
+            <a href='/' ><img src='/wp-content/themes/roys-header/images/dinomiteLogo-85.png'
+             > </a>
+        </td>";
+                }
+                print "
+     </tr></table>
+  </div>
+";
+                break;
+            default:
+                print "<p>E#1304 Unknown switchName of '$switchName.jpg' in header.php</p> ";
+        } // end of switch";
+    } // end if ( $mobile )
+
+
+    print "\n\n
+    <!--  =============================================================================== nav bar -->
+    ";
+    // --------------------------------------------------------- nave bar code
+    switch ($switchName) {
+        case "eriepittsburgh": // --------------------------------------------
+            // eriepittsburgh has menu buried i the swish
+        ?>
+
+            <div id="navbar" class="eriemenu  menucolor" style='z-level:1;'>
+                <nav id="site-navigation" class="navigation main-navigation menucolor">
+                    <table role="presentation">
+                        <tr>
+                            <td>
+                                <h3 class="menu-toggle">
+                                    <?php _e('Menu', 'twentythirteen'); ?>
+                                </h3>
+                                <a class="screen-reader-text skip-link" href="#content" title="<?php esc_attr_e('Skip to content', 'twentythirteen'); ?>">
+                                    <?php _e('Skip to content', 'twentythirteen'); ?>
+                                </a>
+                                <?php
+                                $rrw_trail_menuText = wp_nav_menu(array(
+                                    'theme_location' => 'primary',
+                                    'menu_class' => 'nav-menu menucolor',
+                                    'echo' => false
+                                ));
+                                echo $rrw_trail_menuText;
+                                ?>
+                            </td>
+                        </tr>
+                    </table>
+                </nav>
+            </div>
+            <!-- id=navbar  -->
+    <?php
+            break;
+
+        case "dino":
+            print "<a href='/' ><img src='/wp-content/themes/roys-header/images/dinoLogo.png' width='768' ></a>";
+            break;
+
+        case "dino1":
+        case "clean":
+        case "picture":
+        case "picturedev":
+        case "tailonly":
+        case "theyworking":
+        case "edit":
+            // menu is displayed on top of the header image
+            // or not displayed at all
+            print " <!-- No menu displayed -->";
+            break;
+
+        case "normal":
+        case "linkup":
+        case "tommarellogc":
+            print rrwHeaderMenu();
+            /*
+        ?>
+            <div id="navbar" class="menucolor" style="z-level:1;">
+                <nav id="site-navigation" class="navigation main-navigation menucolor">
+                    <table role="presentation">
+                        <tr class="nav-menu menucolor">
+                            <td>
+                                <h3 class="menu-toggle">
+                                    <?php _e("Menu", "twentythirteen"); ?>
+                                </h3>
+                                <a class="screen-reader-text skip-link" href="#content" title="<?php esc_attr_e("Skip to content", "twentythirteen"); ?>">
+                                    <?php _e("Skip to content", "twentythirteen"); ?>
+                                </a>
+                        <?php
+                        $rrw_trail_menuText = wp_nav_menu(array(
+                            "theme_location" => "primary",
+                            "menu_class" => "nav-menu menucolor",
+                            "echo" => false
+                        ));
+                        print $rrw_trail_menuText . "
+                            </td>
+                       </tr>
+                   </table>
+                </nav>
+            </div>      <!-- id=navbar  -->
+";
+*/
+            break;
+        default:
+            print "
+<p>E#1305 Unknown switchName of '$switchName' in header.php</p>
+";
+    } // end switchName == "eriepittsburgh" )
+    ?>
+
+    <!-- =====================================  end #navbar -->
+    </div>
+    </header>
+    </div>
+    <!-- end dive id="page" -->
+    <!-- #masthead -->
+    <!--  ===== last line -2 of header.php ============================================ main content -->
+    <div id="main" class="site-main">
